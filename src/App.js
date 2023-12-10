ce6e8aa842fe5bcc7fc44815ca5e94ae613c11e3
vikassinghrathor <rathorevikas2809@gmail.com>
2023-12-10 14:31:35 +0530

    seller's admin page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const SellerAdminPage = () => {
+  const [inputData, setinputData] = useState({
+    productId: "",
+    sellingPrice: "",
+    productName: "",
+  });
+
+  const [products, setProducts] = useState([]);
+  const [totalValue, setTotalValue] = useState(0);
+
+  useEffect(() => {
+    const storedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    setProducts(storedProducts);
+    calculateTotalValue(storedProducts);
+  }, []);
+
+  const inputHandler = (event) => {
+    const { name, value } = event.target;
+    setinputData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const addProduct = () => {
+    const { productId, sellingPrice, productName } = inputData;
+
+    if (!productId || !sellingPrice || !productName) {
+      alert("Please fill in all fields");
+      return;
+    }
+
+    const newProduct = {
+      id: productId,
+      price: parseFloat(sellingPrice),
+      name: productName,
+    };
+
+    setProducts([...products, newProduct]);
+    updateLocalStorage([...products, newProduct]);
+
+    // Clear input fields after adding the product
+    setinputData({
+      productId: "",
+      sellingPrice: "",
+      productName: "",
+    });
+  };
+
+  const deleteProduct = (productId) => {
+    const updatedProducts = products.filter((product) => product.id !== productId);
+    setProducts(updatedProducts);
+    updateLocalStorage(updatedProducts);
+  };
+
+  const updateLocalStorage = (data) => {
+    localStorage.setItem("products", JSON.stringify(data));
+    calculateTotalValue(data);
+  };
+
+  const calculateTotalValue = (data) => {
+    const total = data.reduce((acc, product) => acc + product.price, 0);
+    setTotalValue(total);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Seller's Admin Page</h1>
+      <label>Product ID:</label>
+      <input
+        type="number"
+        name="productId"
+        value={inputData.productId}
+        onChange={inputHandler}
+      />
+      <label>Selling Price:</label>
+      <input
+        type="number"
+        name="sellingPrice"
+        value={inputData.sellingPrice}
+        onChange={inputHandler}
+      />
+      <label>Product Name:</label>
+      <input
+        type="text"
+        name="productName"
+        value={inputData.productName}
+        onChange={inputHandler}
+      />
+      <button onClick={addProduct}>Add Product</button>
+
+      <h1>Products</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - ${product.price}
+            <button onClick={() => deleteProduct(product.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Total value Worth of Products: Rs{totalValue.toFixed(2)}</h3>
     </div>
   );
-}
+};
 
-export default App;
+export default SellerAdminPage;
